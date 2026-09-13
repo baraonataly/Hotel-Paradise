36763aeadcff50d364bc26fc36604fa3de44eeb8
Stephany Ramina <stephanyraminaramina@gmail.com>
2026-09-13 18:37:30 -0300

    Implementa classes Hospede e Reserva

--- Hospede.cs
+++ Hospede.cs
@@ -1,3 +1,24 @@
+using System;
 class Hospede{
+    public int Id;
+    public string Nome;
+    public string CPF;
+    public string Telefone;
+
+    public Hospede(int id, string nome, string cpf, string telefone)
+    {
+        Id = id;
+        Nome = nome;
+        CPF = cpf;
+        Telefone = telefone;
+    }
+
+    public void ExibirDados()
+    {
+        Console.WriteLine("ID: " + Id);
+        Console.WriteLine("Nome: " + Nome);
+        Console.WriteLine("CPF: " + CPF);
+        Console.WriteLine("Telefone: " + Telefone);
+    }
 
 }
--- Reserva.cs
+++ Reserva.cs
@@ -1,3 +1,44 @@
+using System;
 class Reserva{
+    public int Id;
+    public Hospede Hospede;
+    public Quarto Quarto;
+    public DateTime DataEntrada;
+    public DateTime DataSaida;
+    public decimal ValorTotal;
 
+    public Reserva(int id, Hospede hospede, Quarto quarto, DateTime dataEntrada, DateTime dataSaida)
+    {
+        Id = id;
+        Hospede = hospede;
+        Quarto = quarto;
+        DataEntrada = dataEntrada;
+        DataSaida = dataSaida;
+
+        CalcularValor();
+    }
+
+    public void CalcularValor()
+    {
+        int dias = (DataSaida - DataEntrada).Days;
+
+        if (dias > 0)
+        {
+            ValorTotal = dias * Quarto.ValorDiaria;
+        }
+        else
+        {
+            ValorTotal = 0;
+        }
+    }
+
+    public void ExibirDados()
+    {
+        Console.WriteLine("ID da reserva: " + Id);
+        Console.WriteLine("Hóspede: " + Hospede.Nome);
+        Console.WriteLine("Quarto: " + Quarto.Numero);
+        Console.WriteLine("Entrada: " + DataEntrada);
+        Console.WriteLine("Saída: " + DataSaida);
+        Console.WriteLine("Valor total: R$ " + ValorTotal);
+    }
 }
